f3d661530660a86376df26423fc3e0d2c8e6ccad
Mandar Salvi <mandarsalvi03@gmail.com>
2025-01-10 16:33:10 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Exit the script as soon as any command fails
+set -e
+
+# Install Python dependencies from requirements.txt
+echo "Installing Python dependencies..."
+pip install --no-cache-dir -r requirements.txt
+
+# Run database migrations (using Flask-Migrate)
+echo "Applying database migrations..."
+flask db upgrade
+
+# Collect static files (if your Flask app is using something like Flask-Assets)
+echo "Collecting static files..."
+flask collect  # If you are using Flask-Assets or similar tools
